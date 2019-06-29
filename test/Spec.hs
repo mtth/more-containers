77447b302c4570d7fb5385f40155a66b2a8a8b89
Matthieu Monsch <matthieu.monsch@gmail.com>
2019-06-28 20:36:57 -0700

    Add cabal file

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,9 +1,9 @@
-{-# LANGUAGE OverloadedLists #-}
+module Main where
+
+import Test.Hspec (hspec)
 
 import Spec.Multimap
 import Spec.Multiset
-
-import Test.Hspec (hspec)
 
 main :: IO ()
 main = hspec $ do
